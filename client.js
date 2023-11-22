77e709afaadd667d3256bf7ceef7b89a4d50c4e6
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-22 16:27:19 -0500

    imported IP and PORT from constants file

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541 // PORT number here,
+    host: IP, // IP address here,
+    port: PORT, // PORT number here,
   });
 
   // interpret incoming data as text
@@ -14,17 +15,7 @@ const connect = function() {
     console.log('Successfully connected to game server');
     conn.write('Name: VIC');
 
-    // const directions = ['Move: up', 'Move: up', 'Move: left', 'Move: left', 'Move: down', 'Move: down', 'Move: down', 'Move: up', 'Move: up', 'Move: left', 'Move: left', 'Move: left', 'Move: up', 'Move: up',];
-
-    // let delay = 500;
-
-    // for (const moves of directions) {
-    //   setTimeout(() => {
-    //     conn.write(moves);
-    //   }, delay);
-    //   delay += 500;
-    // }
-  })
+  });
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
@@ -33,4 +24,4 @@ const connect = function() {
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
