d5572bee5e8b4e361c6df012cae82bf0c9ffc3c9
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-22 14:22:49 -0500

    added print message when conenct to server and sent server name initials

--- client.js
+++ client.js
@@ -10,6 +10,12 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server')
+    conn.write('Name: VIC')
+
+  })
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
