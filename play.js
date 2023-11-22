36b538a78a5865f544bf8272b7787a01b5a45c18
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-22 14:55:49 -0500

    added setUpInput and handleUserInput

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
-const { connect } = require('./client');
+const {connect} = require('./client');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting ...");
 connect();
