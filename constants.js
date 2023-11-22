9530bafb3dd33ad1608392e579ea157d032fc1d8
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-22 16:26:43 -0500

    added file

---
+++ constants.js
@@ -0,0 +1,18 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+
+//Keys
+const MoveUpKey = 'w';
+const MoveLeftKey = 'a';
+const MoveDownKey = 's';
+const MoveRightKey = 'd'; 
+
+
+module.exports = {
+  IP,
+  PORT,
+  MoveUpKey,
+  MoveLeftKey,
+  MoveDownKey,
+  MoveRightKey
+};
