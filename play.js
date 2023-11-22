781fa55fa6f484a0aa3c88e8c83a6d37cacb4d9e
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-22 15:00:53 -0500

    moved setupInput and handleUserInput into new input.js file. Imported setupImput into file

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const {connect} = require('./client');
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on("data", handleUserInput);
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
